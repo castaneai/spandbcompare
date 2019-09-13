c05c2bc2aae977706fb7056b54c9981976db1194
castaneai <castaneai@castaneai.net>
2019-09-13 17:34:56 +0900

    Add compare tests

--- compare.go
+++ compare.go
@@ -1,10 +1,103 @@
 package spanner_compare
 
-import "context"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
 
 type Result interface {
 }
 
-func Compare(ctx context.Context, ds1, ds2 DataSource) (Result, error) {
-	panic("not implemented")
+type Comparator interface {
+	Compare(a, b interface{}) bool
+}
+
+type AsStringComparator struct{}
+
+func (cmp *AsStringComparator) Compare(a, b interface{}) bool {
+	return fmt.Sprintf("%s", a) == fmt.Sprintf("%s", b)
+}
+
+type Diff struct {
+	Rows1Only []*Row
+	Rows2Only []*Row
+	Rows1     []*Row
+	Rows2     []*Row
+}
+
+func (d *Diff) HasDiff() bool {
+	return len(d.Rows1Only) > 0 || len(d.Rows2Only) > 0 || len(d.Rows1) > 0 || len(d.Rows2) > 0
+}
+
+func Compare(rows1, rows2 []*Row, cmp Comparator) (*Diff, error) {
+	rows1Map := rowsToPKMap(rows1)
+	rows2Map := rowsToPKMap(rows2)
+
+	diff := &Diff{}
+	for pks, row1 := range rows1Map {
+		row2, exists2 := rows2Map[pks]
+		if !exists2 {
+			diff.Rows1Only = append(diff.Rows1Only, row1)
+			continue
+		}
+		irow1, irow2, err := intersect(row1, row2, cmp)
+		if err != nil {
+			return nil, err
+		}
+		if irow1 != nil && irow2 != nil {
+			diff.Rows1 = append(diff.Rows1, irow1)
+			diff.Rows2 = append(diff.Rows2, irow2)
+		}
+	}
+	for pks, row2 := range rows2Map {
+		if _, exists1 := rows1Map[pks]; !exists1 {
+			diff.Rows2Only = append(diff.Rows2Only, row2)
+		}
+	}
+	return diff, nil
+}
+
+func intersect(row1, row2 *Row, cmp Comparator) (*Row, *Row, error) {
+	irow1 := &Row{
+		pkColNames:   row1.pkColNames,
+		ColumnValues: make(map[string]ColumnValue),
+	}
+	irow2 := &Row{
+		pkColNames:   row1.pkColNames,
+		ColumnValues: make(map[string]ColumnValue),
+	}
+	if !reflect.DeepEqual(irow1.pkColNames, irow2.pkColNames) {
+		return nil, nil, errors.New("the primary key of pair of rows must be the same")
+	}
+	// always includes the primary keys
+	for _, pkcn := range irow1.pkColNames {
+		irow1.ColumnValues[pkcn] = row1.ColumnValues[pkcn]
+		irow2.ColumnValues[pkcn] = row2.ColumnValues[pkcn]
+	}
+	for cn, cv1 := range row1.ColumnValues {
+		cv2, exists2 := row2.ColumnValues[cn]
+		if !exists2 {
+			irow1.ColumnValues[cn] = cv1
+			continue
+		}
+		if !cmp.Compare(cv1, cv2) {
+			irow1.ColumnValues[cn] = cv1
+			irow2.ColumnValues[cn] = cv2
+		}
+	}
+	// check whether column value has diff excluding primary key
+	if len(irow1.ColumnValues) <= len(irow1.pkColNames) || len(irow2.ColumnValues) <= len(irow2.pkColNames) {
+		return nil, nil, nil
+	}
+	return irow1, irow2, nil
+}
+
+func rowsToPKMap(rows []*Row) map[string]*Row {
+	pkmap := make(map[string]*Row, len(rows))
+	for _, row := range rows {
+		pks := row.PrimaryKey().String()
+		pkmap[pks] = row
+	}
+	return pkmap
 }
---
+++ compare_test.go
@@ -0,0 +1,74 @@
+package spanner_compare
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCompare_NoDiff(t *testing.T) {
+	pks := []string{"id"}
+	rows1 := []*Row{{pks, map[string]ColumnValue{"id": "a", "name": "a"}}}
+	rows2 := []*Row{{pks, map[string]ColumnValue{"id": "a", "name": "a"}}}
+	diff, err := Compare(rows1, rows2, &AsStringComparator{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, false, diff.HasDiff())
+}
+
+func TestCompare_OnlyDiff(t *testing.T) {
+	pks := []string{"id"}
+
+	{
+		rows1 := []*Row{{pks, map[string]ColumnValue{"id": "a", "name": "a"}}}
+		rows2 := []*Row(nil)
+		diff, err := Compare(rows1, rows2, &AsStringComparator{})
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, true, diff.HasDiff())
+		assert.Equal(t, 1, len(diff.Rows1Only))
+		assert.Equal(t, 0, len(diff.Rows2Only))
+		assert.Equal(t, 0, len(diff.Rows1))
+		assert.Equal(t, 0, len(diff.Rows2))
+	}
+
+	{
+		rows1 := []*Row(nil)
+		rows2 := []*Row{{pks, map[string]ColumnValue{"id": "a", "name": "a"}}}
+		diff, err := Compare(rows1, rows2, &AsStringComparator{})
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, true, diff.HasDiff())
+		assert.Equal(t, 0, len(diff.Rows1Only))
+		assert.Equal(t, 1, len(diff.Rows2Only))
+		assert.Equal(t, 0, len(diff.Rows1))
+		assert.Equal(t, 0, len(diff.Rows2))
+	}
+}
+
+func TestCompare_Diff(t *testing.T) {
+	pks := []string{"id"}
+
+	{
+		rows1 := []*Row{{pks, map[string]ColumnValue{"id": "a", "name": "na", "age": 1}}}
+		rows2 := []*Row{{pks, map[string]ColumnValue{"id": "a", "name": "nb", "age": 1}}}
+		diff, err := Compare(rows1, rows2, &AsStringComparator{})
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, true, diff.HasDiff())
+		assert.Equal(t, 0, len(diff.Rows1Only))
+		assert.Equal(t, 0, len(diff.Rows2Only))
+		assert.Equal(t, 1, len(diff.Rows1))
+		assert.Equal(t, 1, len(diff.Rows2))
+		assert.Equal(t, "a", diff.Rows1[0].ColumnValues["id"])
+		assert.Equal(t, "a", diff.Rows2[0].ColumnValues["id"])
+		assert.Equal(t, "na", diff.Rows1[0].ColumnValues["name"])
+		assert.Equal(t, "nb", diff.Rows2[0].ColumnValues["name"])
+		assert.NotContains(t, diff.Rows1[0].ColumnValues, "age")
+		assert.NotContains(t, diff.Rows2[0].ColumnValues, "age")
+	}
+}
